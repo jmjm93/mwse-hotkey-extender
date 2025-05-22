b09d3be1ed8b81dbf23412566a17b7676eccd187
jmjm93 <dd091193@gmail.com>
2025-05-22 19:16:01 +0200

    native keybind detector

--- main.lua
+++ main.lua
@@ -1,7 +1,6 @@
 local config = require("hotkeyExtender.config");
 
-
-local combo = {
+local f2Key = {
     keyCode = tes3.scanCode.F2,
     isAltDown = false,
     isControlDown = false,
@@ -9,9 +8,9 @@ local combo = {
 }
 
 local _STATE = {
-    NORMAL = 0,
-    OPEN = 1,
-    LISTENING = 1
+    NORMAL = 0,     -- menu not opened
+    OPEN = 1,       -- menu opened
+    LISTENING = 1   -- binding a key
 }
 
 local state = _STATE.NORMAL
@@ -27,15 +26,15 @@ local hotkeys = config.hotkeys
 
 local function commitChanges()
     local output = {}
-    for _,hotkey in ipairs(hotkeys) do
-        output[_] = {
+    for i,hotkey in ipairs(hotkeys) do
+        output[i] = {
             object = hotkey.object,
             actionType = hotkey.actionType,
             actionId = hotkey.actionId,
             keyName = hotkey.keyName
         }
     end
-    mwse.saveConfig("hotkeyExtender.config", { hotkeys = output })
+    mwse.saveConfig("hotkeyExtender", { hotkeys = output })
 end
 
 -- helper --
@@ -48,14 +47,50 @@ local function getKeybindKey(value)
     return nil
 end
 
--- BUTTON CALLBACKS --
+
 local function closeMenu(e)
     menu:destroy()
     tes3ui.leaveMenuMode()
     state = _STATE.NORMAL
 end
 
--- MAIN DRAWING METHODS --
+local function renderIconBlock(parent, hotkey)
+    local button = parent:createThinBorder({ id = "icon_block" })
+	button.width = 60
+	button.height = 60
+	button.borderAllSides = 4
+	button.paddingAllSides = 6
+    if(hotkey.action) then
+        local iconPath
+        local borderPath
+        if hotkey.actionType == "spell" then
+            iconPath = "Icons\\s\\B_" .. string.sub(hotkey.action.effects[1].object.icon,3,100)
+            borderPath = "Textures\\menu_icon_select_magic.tga"
+        else
+            iconPath = "Icons\\" .. hotkey.action.icon
+            borderPath = "Textures\\menu_icon_select_magic_magic.tga"
+        end
+		local barterIcon = button:createImage{ path = borderPath }
+		barterIcon.widthProportional = 1
+		barterIcon.heightProportional = 1
+		barterIcon.childAlignX = 0.0
+		barterIcon.childAlignY = 0.0
+		barterIcon.consumeMouseEvents = false
+		local shadowIcon
+		if not hotkey.actionType == "spell" then
+			shadowIcon = barterIcon:createImage{ path = iconPath }
+			shadowIcon.color = {0.0, 0.0, 0.0}
+			shadowIcon.absolutePosAlignX = 0
+			shadowIcon.absolutePosAlignY = 0
+			shadowIcon.borderAllSides = 12
+			shadowIcon.consumeMouseEvents = false
+		end
+		local icon = barterIcon:createImage{ path = iconPath }
+		icon.borderAllSides = 6
+		icon.consumeMouseEvents = false
+	end
+    return button
+end
 
 local function renderHotkeyField()
     if not hotkeyField or not menu then
@@ -63,9 +98,7 @@ local function renderHotkeyField()
     end
     hotkeyField:destroyChildren()
     for _, hotkey in ipairs(hotkeys) do
-        if (_ > 1) then
-            hotkeyField:createDivider()
-        end
+        if (_ > 1) then hotkeyField:createDivider() end
         local hotkeyLabel = hotkeyField:createLabel({
             id = "hotkey-name-btn",
             text = hotkey.object and "Hotkey " .. _ or "New hotkey"
@@ -75,41 +108,39 @@ local function renderHotkeyField()
             text = hotkey.object and string.upper(hotkey.keyName) or "Select key"
         })
         hotkeyButton:register(tes3.uiEvent.mouseClick, function(e)
+            menu.visible = false
             state = _STATE.LISTENING
             listeningKeyPopup = tes3ui.createMenu({ id = "listening-key-popup",
             dragFrame = false,
             fixedFrame = true,
             modal = true,
             loadable = false })
-            listeningKeyPopup:createLabel({ id = "listening-key-popup-text", text = "Press the key you want to bind" })
+            listeningKeyPopup:createLabel({ id = "listening-key-popup-text", text = "Press the key you want to bind..." })
             listeningHotkeyIndex = _
             hotkey._button = hotkeyButton
             hotkey._label = hotkeyLabel
             hotkeyButton.text = "Listening..."
         end)
-        local actionButton = hotkeyField:createButton({
-            id = "action-select-btn",
-            text = (hotkey.action and hotkey.action.name or "Set Action")
-        })
+        local actionButton = renderIconBlock(hotkeyField, hotkey)
         actionButton:register(tes3.uiEvent.mouseClick, function(e)
-            menu.visible = false
-            tes3ui.showMagicSelectMenu({
-                id = "action-selector-menu",
-                title = "Select Action",
-                selectSpells = true,
-                selectPowers = true,
-                selectEnchanted = true,
-                callback = function(e)
-                    menu.visible = true
-                    hotkey.action = e.spell and e.spell or (e.item and e.item or nil)
-                    hotkey.actionId = e.spell and e.spell.id or e.item.id
-                    hotkey.actionType = e.spell and "spell" or "item"
-                    actionButton.text = hotkey.action.name
-                    renderHotkeyField()
-                    if hotkey.action and hotkey.object then commitChanges() end
-                end
-            })
-            end)
+                menu.visible = false
+                tes3ui.showMagicSelectMenu({
+                    id = "action-selector-menu",
+                    title = "Select Action",
+                    selectSpells = true,
+                    selectPowers = true,
+                    selectEnchanted = true,
+                    callback = function(e)
+                        menu.visible = true
+                        hotkey.action = e.spell and e.spell or (e.item and e.item or nil)
+                        hotkey.actionId = e.spell and e.spell.id or e.item.id
+                        hotkey.actionType = e.spell and "spell" or "item"
+                        actionButton.text = hotkey.action.name
+                        renderHotkeyField()
+                        if hotkey.action and hotkey.object then commitChanges() end
+                    end
+                })
+                end)
         local deleteButton = hotkeyField:createButton({
             id = "hotkey-delete-btn",
             text = "Delete"
@@ -120,6 +151,13 @@ local function renderHotkeyField()
         end)
     end
     hotkeyContainer:updateLayout()
+    menu:updateLayout()
+end
+
+local function resetCb(e)
+    hotkeys = {}
+    commitChanges()
+    renderHotkeyField()
 end
 
 local function addCb(e)
@@ -142,7 +180,7 @@ local function renderMenu()
     hotkeyContainer = menu:createVerticalScrollPane{
         id = 'hotkey-container'
     };
-    hotkeyContainer.width = 400
+    hotkeyContainer.width = 130
     hotkeyContainer.heightProportional = nil
     hotkeyContainer.widthProportional = nil
     hotkeyContainer.height = 400
@@ -159,9 +197,7 @@ local function renderMenu()
         id = 'reset-hotkeys-btn',
         text = "Reset all"
     })
-    resetButton:register(tes3.uiEvent.mouseClick, function(e)
-        renderHotkeyField()
-    end)
+    resetButton:register(tes3.uiEvent.mouseClick, resetCb)
     local okButton = menu:createButton({
         id = 'close-btn',
         text = "Finish"
@@ -202,7 +238,7 @@ local function openMenu()
         modal = true,
         loadable = false
     })
-    tes3ui.enterMenuMode(menuId)
+    tes3ui.enterMenuMode(menu.id)
     renderMenu()
     renderHotkeyField()
     renderHotkeyField()
@@ -218,12 +254,20 @@ local function keybindPressed(key)
     end
 end
 
+local function isNativeKeybind(key)
+    for k, keybind in pairs(tes3.keybind) do
+        if key.keyCode == tes3.getInputBinding(keybind).code then
+            return true
+        end
+    end
+end
+
 
 local function main(e)
     setupConfigHotkeys()
     if state == _STATE.NORMAL then
         if not tes3.isKeyEqual({
-            expected = combo,
+            expected = f2Key,
             actual = e
         }) then
             keybindPressed(e)
@@ -233,17 +277,23 @@ local function main(e)
         return
     end
     if state == _STATE.OPEN then
-        if tes3.isKeyEqual({ expected = combo, actual = e }) then
+        if tes3.isKeyEqual({ expected = f2Key, actual = e }) then
             closeMenu(e)
         end
     end
     if state == _STATE.LISTENING then
-        if not listeningHotkeyIndex then
+        if not listeningHotkeyIndex or e.keyCode == tes3.scanCode.lShift then
             return
         end
-        listeningKeyPopup:destroy()
-        listeningKeyPopup = nil
+        if isNativeKeybind(e) then
+            tes3.messageBox("This key is already bound to a native action.")
+            return
+        end
         local listeningHotkey = hotkeys[listeningHotkeyIndex]
+        if(listeningKeyPopup) then listeningKeyPopup:destroy() end
+        listeningKeyPopup = nil
+        menu.visible = true
+        state = _STATE.OPEN
         listeningHotkey.object = e
         listeningHotkey.keyName = getKeybindKey(e.keyCode)
         if listeningHotkey._button then
@@ -252,7 +302,6 @@ local function main(e)
         if listeningHotkey._label then
             listeningHotkey._label.text = "Hotkey " .. listeningHotkeyIndex
         end
-        state = _STATE.NORMAL
         renderHotkeyField()
         if listeningHotkey.action and listeningHotkey.object then commitChanges() end
     end
